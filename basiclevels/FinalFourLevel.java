753657bb8fec145932a7ca0bc5e68d691c8fde73
Michael Ternovsky <58849173+MichaelTernovsky@users.noreply.github.com>
2022-01-12 13:42:00 +0200

    Update FinalFourLevel.java

--- basiclevels/FinalFourLevel.java
+++ basiclevels/FinalFourLevel.java
@@ -9,7 +9,7 @@ import geometry.Rectangle;
 import interfaces.Sprite;
 /**
  * Class FinalFourLevel.
- * @author Michael Ternovsky 316534809
+ * @author Michael Ternovsky
  */
 public class FinalFourLevel implements LevelInformation {
     private int numberOfBalls;
